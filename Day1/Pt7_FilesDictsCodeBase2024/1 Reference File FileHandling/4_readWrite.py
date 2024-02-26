479d24592e0d59f82c825baeb151d0002da0739e
CorneNapier <CorneNapier.bootcamp@justit.co.uk>
2024-02-26 11:56:33 +0000

    Update 4_readWrite.py, 5_userInput.py
    Added notes to 4_readWrite.py, and completed the task set in 5_userInput.py

--- Day1/Pt7_FilesDictsCodeBase2024/1 Reference File FileHandling/4_readWrite.py
+++ Day1/Pt7_FilesDictsCodeBase2024/1 Reference File FileHandling/4_readWrite.py
@@ -22,5 +22,6 @@ with open('Day1/Pt7_FilesDictsCodeBase2024/yourName.txt',"r+") as filePath1:# fo
 # Read the contents from your file (yourName.txt) and write to your file  
 # use the with the correct mode and ensure you use both the read and the write() methods to perform their respective 
 # operations
-
+# a+ = The 'a+' mode opens the file for both reading and writing, positioning the file pointer at the end for writing in existing files and creating a new, empty file if it doesn't exist.
+# w+ = In 'w+' mode, the file is opened for both reading and writing, existing content is cleared, a new empty file is created if it doesn't exist, and the file pointer is positioned at the beginning.
 
--- Day1/Pt7_FilesDictsCodeBase2024/1 Reference File FileHandling/5_userInput.py
+++ Day1/Pt7_FilesDictsCodeBase2024/1 Reference File FileHandling/5_userInput.py
@@ -5,7 +5,8 @@ age      =    int(input("Enter your age: "))
 
 "Make"
 "To Do: Task 1: Use the code above to ask for user input and then save it to a file called userDetails.txt"
-
+with open("Day1/Pt7_FilesDictsCodeBase2024/userDetails.txt", "a") as addData:
+    addData.write('\n' + fname +'\n' + address +'\n' + interest + '\n' + str(age))
 
 
 
